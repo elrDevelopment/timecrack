55247a9826a155d1f2a907a7ea67766973483840
Erick <erick@pop-os.localdomain>
2019-02-21 06:42:53 -0600

    updated git ignore and finished initial modeling

--- main/Model.py
+++ main/Model.py
@@ -3,7 +3,7 @@ from flask import Flask
 from marshmallow import Schema, fields, pre_load, validate
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.dialects.postgresql import UUID, JSON
 from sqlalchemy.orm import sessionmaker, relationship
 from passlib.apps import custom_app_context as pwd_context
 
@@ -14,7 +14,7 @@ db = SQLAlchemy()
 
 
 class Project(db.Model):
-    __tablename__ = 'projects'
+    __tablename__ = 'project'
     id = db.Column(db.Integer, primary_key=True)
     pubid  = db.Column(UUID(as_uuid=True), server_default= db.text("uuid_generate_v4()"), )
     display_name = db.Column(db.String, nullable=False)
@@ -42,10 +42,7 @@ class TimeTracking(db.Model):
     __tablename__ = 'time_tracking'
     id = db.Column(db.Integer, primary_key=True)
     pubid  = db.Column(UUID(as_uuid=True), server_default= db.text("uuid_generate_v4()"), )
-    tracking_type = db.Column(db.Integer) ##specifies what type of tracking is being stored (project = 1, task = 2, sub_task = 3)
-    start_time = db.Column(db.DateTime, default=datetime.datetime.utcnow) 
-    stop_time = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-    description = db.Column(db.String, nullable=True)
+    tracking = db.Column(JSON)
     project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
     task_id = db.Column(db.Integer, db.ForeignKey('task.id'))
     subtask_id = db.Column(db.Integer, db.ForeignKey('subtask.id'))
@@ -55,8 +52,8 @@ class Users(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     pubid  = db.Column(UUID(as_uuid=True), server_default= db.text("uuid_generate_v4()"), )
-    auth = relationship("LocalAuth", uselist=False, back_populates="users")
-    username = db.Column(db.string(80), nullable=False)
+    auth = relationship("LocalAuth", uselist=False, back_populates="user")
+    username = db.Column(db.String(80), nullable=False)
 
 class LocalAuth(db.Model):
     __tablename__ = 'local_auth'
@@ -64,7 +61,7 @@ class LocalAuth(db.Model):
     pubid  = db.Column(UUID(as_uuid=True), server_default= db.text("uuid_generate_v4()"), )
     password_hash = db.Column(db.String(128))
     parent_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    parent = relationship("Users", back_populates="local_auth")
+    parent = relationship("User", back_populates="local_auth")
 
     def hash_password(self, password):
         self.password_hash = pwd_context.encrypt(password)
